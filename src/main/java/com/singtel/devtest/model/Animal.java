e661ce5354e1baf2c3edcd7f5d85d89f20430f6e
HSENID <shdinesh.99@gmail.com>
2020-06-15 00:04:14 +0800

    Q1) a) Bird sing() method implementation with unit test

---
+++ src/main/java/com/singtel/devtest/model/Animal.java
@@ -0,0 +1,8 @@
+package com.singtel.devtest.model;
+
+public class Animal {
+
+    public void walk(){
+        System.out.println("I am walking");
+    }
+}
---
+++ src/test/java/com/singtel/devtest/BirdTest.java
@@ -0,0 +1,33 @@
+package com.singtel.devtest;
+
+import com.singtel.devtest.model.Bird;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class BirdTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+
+    @Test
+    public void singTest() {
+        new Bird().sing();
+        String expected = "I am singing" + System.lineSeparator();
+        Assert.isTrue(expected.equals(outContent.toString()), "Bird can't sing");
+    }
+}
